2711d42dcde142d0682ae8bf287418052dfa7e2c
QLug Chain <qlugchain@gmail.com>
2020-08-18 21:23:22 +0700

    first commit

---
+++ src/components/header/HeaderSettings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex items-center flex-none px-3 py-3 border-b-2 border-transparent cursor-pointer text-settings-icon md:px-2 md:py-4 mt-2px hover:border-theme-accents hover:text-blue transition"
+    @click="toggleModal"
+  >
+    <SvgIcon name="gear" view-box="0 0 16 20" />
+    <SettingsModal v-if="showModal" @close="toggleModal" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { SettingsModal } from "@/components/modals";
+
+@Component({
+  components: {
+    SettingsModal,
+  },
+})
+export default class HeaderSettings extends Vue {
+  private showModal = false;
+
+  public toggleModal() {
+    this.showModal = !this.showModal;
+  }
+}
+</script>
---
+++ src/config/index.ts
@@ -0,0 +1,88 @@
+export const I18N = {
+  defaultLocale: "en-GB",
+  enabledLocales: ["en-GB", "en-US", "it-IT", "fr-FR", "nl-NL", "pl-PL", "pt-BR"],
+  languages: {
+    "en-GB": "English (UK)",
+    "en-US": "English (US)",
+    "it-IT": "Italiano",
+    "fr-FR": "Français",
+    "nl-NL": "Nederlands",
+    "pl-PL": "Polski",
+    "pt-BR": "Português (BR)",
+  },
+};
+
+export const brand = {
+  footer: {
+    url: "https://homepage.io",
+    text: "homepage.io",
+  },
+};
+
+export const knownWalletsUrls = {
+  // mainnet: "https://raw.githubusercontent.com/ArkEcosystem/common/master/mainnet/known-wallets.json",
+  // devnet: "https://raw.githubusercontent.com/ArkEcosystem/common/master/devnet/known-wallets.json",
+};
+
+export const socialMedia = [
+  {
+    name: "facebook",
+    url: "https://facebook.margix.org",
+    viewBox: "0 0 10 16",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.margix.org",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "youtube",
+    url: "https://youtube.margix.org",
+    viewBox: "0 0 18 16",
+  },
+  {
+    name: "email",
+    url: "mailto:info@margix.org",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "telegram",
+    url: "https://telegram.me",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "reddit",
+    url: "https://reddit.margix.org",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "github",
+    url: "https://github.margix.org",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "bitcointalk",
+    url: "https://bitcointalk.margix.org",
+    viewBox: "0 0 16 16",
+  },
+  {
+    name: "slack",
+    url: "https://slack.margix.org",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "discord",
+    url: "https://discord.margix.org",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "instagram",
+    url: "https://instagram.margix.org",
+    viewBox: "0 0 14 16",
+  },
+  {
+    name: "medium",
+    url: "https://blog.margix.org",
+    viewBox: "0 0 16 16",
+  }
+];
